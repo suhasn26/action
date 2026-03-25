0de478da1eb6eaf7e00d1af07afc1e3a9f3dc3de
suhasn26 <suhasnaik229@gmail.com>
2026-03-25 07:00:41 +0000

    java

---
+++ Add.java
@@ -0,0 +1,9 @@
+public class Add {
+   public static void main(String[] args) {
+       int first = 10;
+       int second = 20;
+       // Add two numbers
+       int sum = first + second;
+       System.out.println(first + " + " + second + " = " + sum);
+   }
+}
